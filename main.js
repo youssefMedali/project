21a7178c71e30f406dc53a655620d6cbd35c5026
baklouti118 <bakloutimedali118@gmail.com>
2021-08-11 09:33:56 +0100

    done

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 var check = $('#checkBtn');
-check.one('click',(function(){
- $('#result').append(function compare(){
- 	           var inputValue = $('#inp').val();
-	            if(inputValue==="Johnny English" || inputValue==="johnny english"){
-	         	return "Congratulation! Correct Answer!";
-	         }    
-	         return "Sorry,try again..." 
-            });
+check.click(function(){
+ if($('#inp').val() === "Johnny English" || $('#inp').val() === "johnny english"){
 
-}));
+$('#result').text("Congratulation! Correct Answer!");
+$('#result').append('<img class="movieImg" src="https://flxt.tmsimg.com/assets/p32279_p_v10_at.jpg">');
+$('.movieImg').css({"width": "200px" , "position": "relative", "bottom": "-20px" });;
+ }
+
+ else if($('#inp').val()!=="Johnny English" || $('#inp').val()!=="johnny english"){
+    $('#result').text("Sorry,try again..." )
+    $('#result').css("text-indent", "150px")
+}
+
+});
 
 
 var trueResult = $('#resultBtn');
-trueResult.one('click',(function(){
- $('#result').append("The true result is 'Johnny English'")
+trueResult.one('click',(function(){ 
+   $('#result').text("")
+ $('#result').append($("<p id='para'>The true result is 'Johnny English'</p>"))
+ $('#result').append('<img class="movieImg" src="https://flxt.tmsimg.com/assets/p32279_p_v10_at.jpg">');
+ $('.movieImg').css({"width": "200px" , "position": "relative", "bottom": "-20px", "left": "50px" });
+ $('#result').css({"color":"red","position": "relative", "left": "-30px", "top": "30px"});
 }))
-trueResult.click(function(){
- $('#result').css("color","red")
-});
+
+ 
--- main2.js
+++ main2.js
@@ -1,22 +1,27 @@
 var check = $('#checkBtn');
-check.one('click',(function(){
- if($('#inp').val() === "Johnny English"){
+check.click(function(){
+ if($('#inp').val() === "Titanic" || $('#inp').val() === "titanic" ){
 
 $('#result').text("Congratulation! Correct Answer!");
+$('#result').append('<img class="movieImg" src="https://fr.web.img2.acsta.net/pictures/19/10/25/11/18/5224976.jpg">');
+$('.movieImg').css({"width": "200px" , "position": "relative", "bottom": "-20px" });
+$('#result').css("text-indent", "30px")
 
  }
 
- else if($('#inp').val()!=="Johnny English"){
+ else if($('#inp').val()!=="Titanic"){
     $('#result').text("Sorry,try again..." )
+    $('#result').css("text-indent", "150px")
 }
 
 });
 
 
 var trueResult = $('#resultBtn');
-trueResult.click(function(){
- $('#result').append("The true result is 'Titanic'")
-})
-trueResult.click(function(){
- $('#result').css("color","red")
-})
+trueResult.one('click',(function(){
+   $('#result').text("")
+ $('#result').append($("<p id='para'>The true result is 'Titanic'</p>"))
+ $('#result').append('<img class="movieImg" src="https://fr.web.img2.acsta.net/pictures/19/10/25/11/18/5224976.jpg">');
+ $('.movieImg').css({"width": "200px" , "position": "relative", "bottom": "10px", "left": "150px", "display": "block"});
+ $('#result').css({"color":"red", "text-indent":"80px", "position": "absolute", "top": "10px"})
+}))
