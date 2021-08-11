7aa56b80c5f835dd8c7f436604007a3e4a8a317c
Youssef-Bedoui <87306792+Youssef-Bedoui@users.noreply.github.com>
2021-08-11 10:02:49 +0100

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,5 +1,4 @@
 var check = $('#checkBtn');
-
 check.click(function(){
  if($('#inp').val() === "Titanic" || $('#inp').val() === "titanic" ){
 
@@ -13,25 +12,6 @@ $('#result').css("text-indent", "30px")
  else if($('#inp').val()!=="Titanic"){
     $('#result').text("Sorry,try again..." )
     $('#result').css("text-indent", "150px")
-
-
-check.click(function(){
- if($('#inp').val() === "Titanic" || $('#inp').val() === "titanic" ){
-
-check.one('click',(function(){
- if($('#inp').val() === "Johnny English"){
-
-$('#result').text("Congratulation! Correct Answer!");
-
- }
-
-
- else if($('#inp').val()!=="Titanic"){
-
- else if($('#inp').val()!=="Johnny English"){
-    $('#result').text("Sorry,try again..." )
-
-
 }
 
 });
@@ -39,19 +19,9 @@ $('#result').text("Congratulation! Correct Answer!");
 
 var trueResult = $('#resultBtn');
 trueResult.one('click',(function(){
-
 	$('#result').text("")
-
-
-   $('#result').text("")
-
  $('#result').append($("<p id='para'>The true result is 'Titanic'</p>"))
  $('#result').append('<img class="movieImg" src="https://fr.web.img2.acsta.net/pictures/19/10/25/11/18/5224976.jpg">');
  $('.movieImg').css({"width": "200px" , "position": "relative", "bottom": "10px", "left": "150px", "display": "block"});
  $('#result').css({"color":"red", "text-indent":"80px", "position": "absolute", "top": "10px"})
-
-}))
-
- $('#result').append("The true result is 'Titanic'")
-
 }))
